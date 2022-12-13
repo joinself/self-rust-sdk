666a09e75f2372a92721d5e713e9db65d8b67aa2
Tom Bevan <tom.bevan@r3labs.io>
2022-12-13 18:36:08 +0000

    flatbuffer protocol

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 pub mod error;
 pub mod keypair;
 pub mod message;
-pub mod protocol;
+pub mod protocol {
+    mod aclcommand;
+    pub use self::aclcommand::*;
+    mod err_type;
+    pub use self::err_type::*;
+    mod msg_type;
+    pub use self::msg_type::*;
+    mod msg_sub_type;
+    pub use self::msg_sub_type::*;
+}
 pub mod siggraph;
 pub mod time;
 pub mod transport;
