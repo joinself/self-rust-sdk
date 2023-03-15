c043fd8244d27f4644a296998a1134a9bdbf3f8c
Tom Bevan <tom@joinself.com>
2023-03-15 12:32:40 +0000

    State Management - Basic (#10)
    * basic storage interface using sqlite

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
+pub mod account;
 pub mod crypto;
 pub mod error;
 pub mod keypair;
 pub mod message;
+pub mod messaging;
 pub mod protocol;
 pub mod siggraph;
+pub mod storage;
 pub mod time;
 pub mod transport;
