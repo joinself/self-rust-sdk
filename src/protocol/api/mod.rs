5be2d6d7b3fd011ae511f150e9bd289768b97582
Tom Bevan <tom.bevan@r3labs.io>
2023-06-28 22:40:56 +0400

    group creation

--- src/protocol/api/mod.rs
+++ src/protocol/api/mod.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use crate::error::SelfError;
+use crate::{error::SelfError, identifier::Identifier};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct PrekeyResponse {
@@ -10,5 +10,26 @@ pub struct PrekeyResponse {
 impl PrekeyResponse {
     pub fn new(body: &[u8]) -> Result<Self, SelfError> {
         ciborium::de::from_reader(body).map_err(|_| SelfError::RestResposeBodyInvalid)
+    }
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct KeyCreateRequest {
+    pub identifier: Vec<u8>,
+}
+
+impl KeyCreateRequest {
+    pub fn encode(identifier: &Identifier) -> Result<Vec<u8>, SelfError> {
+        let mut buffer = Vec::new();
+
+        // TODO update error
+        ciborium::ser::into_writer(
+            &KeyCreateRequest {
+                identifier: identifier.id(),
+            },
+            &mut buffer,
+        )
+        .map_err(|_| SelfError::MessageEncodingInvalid)?;
+
+        Ok(buffer)
     }
 }
